465e294c0d0ea6fd8b25453c7ba62bf167fb1a90
Harish Prasad <hp.harishprasad@gmail.com>
2022-02-05 12:21:23 +0530

    initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+var color = "Red";
+var userName = "Smileys Emoji Interpreter";
+
+var emojiDictionary = {
+  "😀": "Grinning Face",
+  "😉": "Winking Face",
+  "😁": "Grinning Face with Smiling Eyes",
+  "😆": "Smiling Face with Open Mouth and Tightly-Closed Eyes",
+  "😅": "Smiling Face with Open Mouth and Cold Sweat ",
+  "😂": " Face with Tears of Joy",
+  "🤣": "Rolling On The Floor Laughing",
+  "😊": "Smiling Face with Smiling Eyes",
+  "🙂": "Slightly Smiling Face",
+  "😄": "Smiling Face with Open Mouth and Smiling Eyes",
+  "🙃": "Upside-Down Face",
+  "😇": "Smiling Face with Halo",
+  "😃": "Smiling Face with Open Mouth"
+};
+
+var emojiWeKnows = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  function inputHandler(event) {
+    var currentEmoji = event.target.value;
+    var meaning = emojiDictionary[currentEmoji];
+    if (meaning === undefined) {
+      setInputValue(`Sorry! we don't have ${currentEmoji} in our Database :( `);
+    } else {
+      setInputValue(meaning);
+    }
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setInputValue(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Welcome To <span style={{ color }}>{userName}</span>
+      </h1>
+      <input placeholder="Type your emoji here..." onChange={inputHandler} />
+      <div className="output">{inputValue}</div>
+
+      {emojiWeKnows.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            className="displayEmoji"
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
